81e060bb0e78f3b6b2a6a821b74ddc58e3a5d6f7
Matheus Henrique <teus.henrique021@gmail.com>
2026-01-27 18:51:36 -0300

    Criação de novo db

--- myapp/models.py
+++ myapp/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=100)
     phone = models.IntegerField(max_length=11)
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_value = models.DecimalField(max_digits=10, decimal_places=2)
