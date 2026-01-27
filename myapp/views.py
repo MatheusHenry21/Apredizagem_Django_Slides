9105e1d6a7712bfaa4c6bcdd0f44fee173958c3b
Matheus Henrique <teus.henrique021@gmail.com>
2026-01-27 19:32:41 -0300

    Conecção web ao app

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def teste(request: HttpRequest):
+    return HttpResponse('Vamos corinthians')
