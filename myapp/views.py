e0f8552a26b3ff6ceaf0a703495c974f9403a735
Matheus Henrique <teus.henrique021@gmail.com>
2026-01-28 20:18:52 -0300

    Criação de uma QuerySet com Count

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.db.models import Count
 from . import models
 
 def teste(request: HttpRequest):
-    return HttpResponse(models.Order.objects.select_related('client'))
+    return HttpResponse(models.Order.objects.select_related('client').all())
+
+def total_clientes(request: HttpRequest):
+    client = models.Order.objects.annotate(totalClient=Count('client'))
