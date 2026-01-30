98f703ab9b9ff58e72daa4ff6b7ad1f5cfc1e17f
Matheus Henrique <teus.henrique021@gmail.com>
2026-01-30 15:20:56 -0300

    Criação de templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest
 from django.db.models import Count
 from . import models
 
 def teste(request: HttpRequest):
-    return HttpResponse(models.Order.objects.select_related('client').all())
+    return render(request, 'index.html')
 
 def total_clientes(request: HttpRequest):
     client = models.Order.objects.annotate(totalClient=Count('client'))
